7989298f613453de4f7a98459013e86d17896604
amirdivanbeigi <32493593+amirdivanbeigi@users.noreply.github.com>
2018-06-07 13:15:39 +0430

    6time

--- git test/ViewController.swift
+++ git test/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("bye world")
+        print("nana")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
